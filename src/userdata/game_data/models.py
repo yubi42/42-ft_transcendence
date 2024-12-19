b9156ea622b762040821588657e41bdc359c0a47
Thomas Ho <thofting@student.42berlin.de>
2024-12-19 16:41:19 +0100

    Added validation for the game data model

--- src/userdata/game_data/models.py
+++ src/userdata/game_data/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.utils.timezone import now
 
 class GameData(models.Model):
     # game modes are 'single-player' or 'multi-player'
-    gameMode = models.CharField(max_length=10)
+    gameMode = models.CharField(max_length=15)
     player1 = models.CharField(max_length=50)
-    player2 = models.CharField(max_length=50)
+    player2 = models.CharField(max_length=50, default=None)
+    gameFinished = models.BooleanField(default=True)
     # score for example '1-12' 
     score = models.CharField(max_length=20)
-    dateTime = models.DateTimeField()
+    finishTime = models.DateTimeField()
     
     def __str__(self):
         return (str(self.dateTime) + ':' + str(self.player1) + ' vs. ' 
--- src/userdata/game_data/serializers.py
+++ src/userdata/game_data/serializers.py
@@ -1,7 +1,34 @@
 from rest_framework import serializers
 from .models import GameData
+from django.contrib.auth.models import User
+from app.settings import GAME_MODES
+import re
 
 class GameDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GameData
-        fields = '__all__'
+	class Meta:
+		model = GameData
+		fields = '__all__'
+	
+	def validate_gameMode(self, value):
+		valid_modes = GAME_MODES
+		if value not in valid_modes:
+			raise serializers.ValidationError("Invalid gameMode. Valid options are: " +", ".join(valid_modes))
+		return value
+	
+	def validate_score(self, value):
+		if not re.match(r'^\d+-\d+$', value):
+			raise serializers.ValidationError(
+				"Invalid score, must be in format 'number-number' (e.g., '1-12').")
+		return value
+	
+	def validate(self, data):
+		if not User.objects.filter(username=data['player1']):
+			raise serializers.ValidationError({"player1": 
+				f"Player {data['player1']} does not exist."})
+		if data['gameMode'] == 'multi-player':
+			if not User.objects.filter(username=data['player2']):
+				raise serializers.ValidationError({"player2": 
+					f"Player {data['player2']} does not exist."})
+			if data['player1'] == data['player2']:
+				raise serializers.ValidationError(
+					"player1 and player2 cannot be the same.")
