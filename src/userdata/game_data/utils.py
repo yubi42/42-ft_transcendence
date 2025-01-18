64c1d41de3e994817733f7f2021495f7cee08f44
Thomas Ho <thofting@student.42berlin.de>
2025-01-18 19:17:41 +0100

    Added elo score function

--- src/userdata/game_data/utils.py
+++ src/userdata/game_data/utils.py
@@ -1,2 +1,5 @@
-def calculate_elo(players, scores):
-	pass
+# Calculations based on the work of Elo as described in https://en.wikipedia.org/wiki/Elo_rating_system
+def updateEloScore(playerRanking, opponentRanking, gameResult):
+	expectedResult = 1 / (1 + 10**((opponentRanking - playerRanking)/400))
+	newRating = max(0, playerRanking + 10 * (gameResult - expectedResult))
+	return int(newRating)
